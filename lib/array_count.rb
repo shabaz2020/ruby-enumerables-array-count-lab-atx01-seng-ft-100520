00360a85fdbb1099905ae84197e1e7fa27f8541e
scruffy-prompt-3476 <shabaz80@live.com>
2020-09-23 17:32:46 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,7 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   array = [1, "hello", [], 5.01, "world", :name, { a: 1 }]
- 
+  puts array.count
 end
 
 end
